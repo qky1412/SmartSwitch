a71935b5ca2a1449078646fc10ae4ea279c5a092
qky1412@gmail.com <qky911019>
2016-02-25 12:35:46 +0800

    初试操作页

--- js/main.js
+++ js/main.js
@@ -41,6 +41,15 @@ function showButton(flag) {
             showTitle: false
         });
 }
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 /**
  * 电器布局页面所需js
  */
