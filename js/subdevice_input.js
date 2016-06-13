5a835b0103b5aadfd56963b89baa4789fd041197
qky1412 <qky1412@gmail.com>
2016-06-13 21:15:17 +0800

    增加子设备刷新状态接口

--- js/subdevice_input.js
+++ js/subdevice_input.js
@@ -12,10 +12,24 @@ function initData() {
         function (suc) {
             // 执行初始化的回调
             alert("输入子设备初始化" + JSON.stringify(suc));
-            refreshData();
+            refreshInputDeviceData();
         });
 }
-//刷新输入设备各个插槽数据
-function refreshData() {
-
+//刷新输出设备各个插槽数据
+function refreshInputDeviceData() {
+    //查询驱动子设备状态（07H）,APP端查询关联驱动子设备当前工作状态
+    //business_cb:function(true,{cmdCode,numberOfDrivers,driverStates,subdevpid})
+    //cmdCode:命令字, numberOfDrivers:有效驱动数,driverStates:驱动状态集合,subdevpid子设备pid
+    //      or function(false,err);
+    //驱动 状态:
+    //bit7:为1时，有新触发信号时反转整个字节；为0时，依据bit6~bit0数值执行；
+    //bit6~bit0:当前驱动口输出灰度，x1111111-最大输出（最亮），x0000000-最小输出（不亮），
+    //    中间值对应不同的灰度（亮度）。
+    CloudApi.queryInputDevStatus(function(result, data) {
+        if(result) {
+            alert("查询输入设备状态成功" + JSON.stringify(data));
+        }else {
+            alert("查询输入设备状态失败");
+        }
+    });
 }
--- js/subdevice_output.js
+++ js/subdevice_output.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(document).on("pageInit", "#page_subdevice_output", function (e, id, page) {
         JDSMART.ready(function () {
             initData();
+
         });
     });
     $.init();
@@ -11,11 +12,26 @@ function initData() {
     JDSMART.io.initDeviceData(
         function (suc) {
             // 执行初始化的回调
-            alert("输出子设备初始化" + JSON.stringify(suc));
-            refreshData();
+
+            refreshOutputDeviceData();
         });
 }
-//刷新输入设备各个插槽数据
-function refreshData() {
-
+//刷新输出设备各个插槽数据
+function refreshOutputDeviceData() {
+    //查询驱动子设备状态（07H）,APP端查询关联驱动子设备当前工作状态
+    //business_cb:function(true,{cmdCode,numberOfDrivers,driverStates,subdevpid})
+    //cmdCode:命令字, numberOfDrivers:有效驱动数,driverStates:驱动状态集合,subdevpid子设备pid
+    //      or function(false,err);
+    //驱动 状态:
+    //bit7:为1时，有新触发信号时反转整个字节；为0时，依据bit6~bit0数值执行；
+    //bit6~bit0:当前驱动口输出灰度，x1111111-最大输出（最亮），x0000000-最小输出（不亮），
+    //    中间值对应不同的灰度（亮度）。
+    CloudApi.queryOutputDevStatus(function(result, data) {
+        alert("查询输出设备状态" + JSON.stringify(data));
+        if(result) {
+            alert("查询输出设备状态成功" + JSON.stringify(data));
+        }else {
+            alert("查询输出设备状态失败");
+        }
+    });
 }
